4b24876da3c4f902067d75523cb4ac5af07548c3
denisquintasluiz <dennysluizquintas991@hotmail.com>
2024-05-27 20:21:14 +0000

    feat: maked connection with database using prismaORM

---
+++ prisma/migrations/20240527201838_table_employer/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Employer" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Employer_pkey" PRIMARY KEY ("id")
+);
